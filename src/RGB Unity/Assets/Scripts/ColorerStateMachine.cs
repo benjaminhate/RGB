a57491f11c9a45f9bc64dac212b52a47b2635a63
benjamin <benjamin.hate.09@gmail.com>
2019-10-02 17:40:46 +0200

    Added new sprites and animations + changed Colorer

---
+++ src/RGB Unity/Assets/Scripts/ColorerStateMachine.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ColorerStateMachine : StateMachineBehaviour
+{
+    private static readonly int RandomDripAnimator = Animator.StringToHash("RandomDrip");
+
+    // OnStateExit is called before OnStateExit is called on any state inside this state machine
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetFloat(RandomDripAnimator, Random.Range(0,3));
+    }
+}
