f273028cd7763fc934992dd8fb492aeb8a397ba3
benjaminhate <benjamin.hate.09@gmail.com>
2020-08-11 17:15:58 +0200

    Correction de la MainCamera et ajout d'un script Editor pour le MainCameraController

---
+++ src/RGB Unity/Assets/Scripts/Editor/MainCameraControllerEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(MainCameraController))]
+    public class MainCameraControllerEditor : UnityEditor.Editor
+    {
+        [MenuItem("Tools/Update Main Camera Controller")]
+        public static void MenuUpdateColorElements()
+        {
+            var cameras = FindObjectsOfType<MainCameraController>();
+            foreach (var camera in cameras)
+            {
+                RefreshPosition(camera);
+            }
+        }
+        
+        public void OnSceneGUI()
+        {
+            RefreshPosition((MainCameraController) target);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            RefreshPosition((MainCameraController) target);
+            base.OnInspectorGUI();
+        }
+
+        private static void RefreshPosition(MainCameraController mainCameraController)
+        {
+            var player = mainCameraController.player;
+            if (player == null) return;
+
+            var playerPosition = player.transform.position;
+            mainCameraController.gameObject.transform.position = new Vector3(playerPosition.x, playerPosition.y, -10f);
+        }
+    }
+}
